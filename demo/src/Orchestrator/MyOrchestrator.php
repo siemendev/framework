5376c5a2e2f3a94ca594154a15a7f044b42a855d
Patrick Siemen <post@patrick-siemen.de>
2023-03-14 09:56:26 +0100

    Update MyOrchestrator.php

--- demo/src/Orchestrator/MyOrchestrator.php
+++ demo/src/Orchestrator/MyOrchestrator.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Factory;
+namespace App\Orchestrator;
 
 use Framework\Orchestrator\Annotation\AnnotatedOrchestrator;
 use Framework\Orchestrator\Annotation\Orchestrator;
